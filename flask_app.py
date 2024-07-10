9c48d3e020bf6bacc1fa903afe29f105f993c798
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-10 15:38:35 -0700

    updated

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,24 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask,request
+import git
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Glory5 Flask Updated Site!'
-
+    
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400   
+            
 if __name__ == '__main__':
     app.run(debug=True)
