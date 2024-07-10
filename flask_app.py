db52dcd13bd741af594f7f2491cea6496964a203
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-10 15:40:26 -0700

    updated

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Glory5 Flask Updated Site!'
+    return 'Glory5 Flask Site!'
     
 @app.route('/update_server', methods=['POST'])
 def webhook():
