8f2a52ac6cd50599fac14bb048918a3f25651f21
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-10 08:14:56 -0700

    updated

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Glory5 Flask Site!'
+    return 'Glory5 Flask Updated Site!'
 
 if __name__ == '__main__':
     app.run(debug=True)
