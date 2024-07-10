3f210670f73f46d095ef4e172f870574318ccf63
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-10 16:27:53 -0700

    updated

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Local push to Glory5 Flask Site!'
+    return 'Push LOCAL to Glory5 Flask Site!'
     
 @app.route('/update_server', methods=['POST'])
 def webhook():
